5fc1b3e67317f5cd79dc1bd613a997c278bf3121
hannahhan153 <hannah.han@biola.edu>
2022-02-07 16:26:55 -0800

    create function to receive input and display data dynamically

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 // slice is an array method that returns a brand-new array based on process.argv starting at third index and ending with final index
 console.log(profileDataArgs);
 
+
+const generatePage = (userName, githubName) => {
+    return `
+        Name: ${userName}
+        Github: ${githubName}
+    `;
+};
+console.log(generatePage('Hannah', 'hannahhan153'));
+
 // arrow does not require function keyword
 // implicit return means we don't have to use the return statement if we're performing one action
 // const never reassigned, let can be reassigned; both are block-scope variables; the let variables are inside block and outside block are unaffected by each other
-const printProfileData = profileDataArr => {
+/* const printProfileData = profileDataArr => {
     // This...
     for (let i = 0; i < profileDataArr.length; i += 1) {
         console.log(profileDataArr[i]);
@@ -15,10 +24,10 @@ const printProfileData = profileDataArr => {
     console.log('================');
 
     // Is the same as this...
-    // accepts a function as an argument and executes that function on each element of the array, using the value of the element at that iteration as its argument
+    // array method, we can't use if there is no array involved in the iterable functionality
     // arrow function 
     profileDataArr.forEach(profileItem => 
         console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs); */
