056d13b2853e8c9e587a42975e8d148c6174ab01
hannahhan153 <hannah.han@biola.edu>
2022-02-07 17:10:30 -0800

    modularize app by moving HTML generator into own file

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
 // allows the file to access the fs module's functions through the fs assignment
 const fs = require('fs');
+const generatePage = require('./src/page-template');
 // argv property of process is array that holds exactly what was typed in command line to capture data and use in app
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
 // slice is an array method that returns a brand-new array based on process.argv starting at third index and ending with final index
+const profileDataArgs = process.argv.slice(2);
 
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-    </head>
-        
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
+const [name, github] = profileDataArgs;
+
 // file name, data written in this case HTML string template, handle error and success message
 fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+    if (err) throw new Error(err);
 
     console.log('Portfolio complete! Check out index.html to see the output.')
 })
