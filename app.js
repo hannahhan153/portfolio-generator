6b695172d25948cb9a59317a2e63c3c959ea4e10
hannahhan153 <hannah.han@biola.edu>
2022-02-12 21:25:11 -0800

    add promises for makefile and copyfile

--- app.js
+++ app.js
@@ -141,10 +141,20 @@ promptUser()
         const pageHTML = generatePage(portfolioData);
 
         // file name, data written in this case HTML string template, handle error and success message
-        fs.writeFile('index.html', pageHTML, err => {
-            if (err) throw new Error(err);
-
+        fs.writeFile('./dist/index.html', pageHTML, err => {
+            if (err) {
+                console.log(err);
+                return;
+            }
             console.log('Page created! Check out index.html to see the output.')
+
+            fs.copyFile('./src/style.css', './dist/style.css', err => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log('Style sheet copied successfully!');
+            });
         });
 
     });
---
+++ utils/generate-site.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const writeFile = fileContent => {
+    // new is a promise object that acts like a container that allows us to run code which at some point will be in a status of pending which means the code has begun to run but waiting for a response
+    // resolve function when code executes successfully and reject function when code fails to execute
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            // if there's an error, reject the Promise and send the error to the Promise's .catch() method
+            if (err) {
+                reject(err);
+                // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+                return;
+            }
+
+            // if everything went well, resolve the Promise and send the successful data to the .then() method
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+const copyFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File copied!'
+            });
+        });
+    });
+};
